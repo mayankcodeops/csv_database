8dabd0b0175285e54e08b6010834705a9ee69108
mayankcodeops <mayank@codeops.tech>
2021-12-15 12:25:19 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        message = "Hello, World!"
+        self.wfile.write(bytes(message, "utf8"))
+
+
+with HTTPServer(('', 8000), Handler) as server:
+    server.serve_forever()
